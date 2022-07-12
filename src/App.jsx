75f0a7a484309997383cecf1c4671ec7ddce537a
Ishita tomar <ishitatomar017@gmail.com>
2022-07-12 14:42:49 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    //<>this is react fragment , it is used when you need to wrap content and don't want to use div
+    //component should return signle element so react fra gment is used .
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+};
+export default App;
