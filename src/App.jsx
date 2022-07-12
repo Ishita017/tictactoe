872343e9e8a138c09c4376f83291e7c94ca32de3
Ishita tomar <ishitatomar017@gmail.com>
2022-07-12 16:22:28 +0530

    added SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React from 'react';
+import Board from './Components/Board';
+import './styles/root.scss';
 const App = () => {
+  // return (
+  //   //<>this is react fragment , it is used when you need to wrap content and don't want to use div
+  //   //component should return signle element so react fra gment is used .
+  //   <>
+  //     <h1>Welcome to React Vite Micro App!</h1>
+  //     <p>Hard to get more minimal than this React app.</p>
+  //   </>
+  // );
+
   return (
-    //<>this is react fragment , it is used when you need to wrap content and don't want to use div
-    //component should return signle element so react fra gment is used .
-    <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
   );
 };
 export default App;
